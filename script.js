52d71b4b6aa35246568d6400ea38cac96d6db1af
24lukeaseeley <24lukeaseeley@gmail.com>
2024-10-11 16:29:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function downloadData() {
+  event.prreventDefault();
+  var startdate = document.getElementById("startdate").value;
+  var enddate = document.getElementById("enddate").value;
+  alert(startdate);
+  alert(enddate);
 
+}
